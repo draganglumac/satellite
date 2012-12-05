7364dd23c6e0940e6d3cdb26d69e38cdb8f7225a
AlexsJones <alexsimonjones@gmail.com>
2012-12-05 11:07:54 +0000

    update

--- src/responder.c
+++ src/responder.c
@@ -31,5 +31,16 @@ void write_result(char* job_id)
 	jnx_sql_query(cp,_s);
 	
 	
+
+	char result_one[256] = "INSERT INTO `AUTOMATION`.`results` (`id`,`testresult`,`jobs_id`,`jobs_machines_machine_id`)VALUES(NULL,'FAIL',";
 	
+	char* cp_two = strdup(result_one);
+	strcat(cp_two,job_id);
+	strcat(cp_two,",(select machines_machine_id from `AUTOMATION`.`jobs` where id=");
+	strcat(cp_two,job_id);
+	strcat(cp_two,"));");
+	
+	printf("Writing to results table -> %s\n",cp_two);
+	//INSERT INTO `AUTOMATION`.`results` (`id`,`testresult`,`jobs_id`,`jobs_machines_machine_id`)VALUES(NULL,"FAIL",2,(select machines_machine_id from `AUTOMATION`.`jobs` where id=2));
+
 }
