947beb3ba5c41cd6ff104ef922f49a60b1870edf
alexsimonjones@gmail.com <alexsimonjones@gmail.com>
2013-03-26 21:55:53 +0000

    updated

--- src/interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef __INTERFACE_H__
-#define __INTERFACE_H__
-#include "jnxsql_interface.h"
-#include "interface_commands.h"
-int perform_job_cycle(void);
-int perform_store_sql_credentials(char* host_addr, char* username, char* pass);
-#endif
