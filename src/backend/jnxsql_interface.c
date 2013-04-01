961837ea3ee1066a9de5d1be6e7a7af8baad197a
AlexsJones <alexsimonjones@gmail.com>
2013-04-01 20:32:31 +0000

    refactoring

--- src/backend/jnxsql_interface.c
+++ src/backend/jnxsql_interface.c
@@ -10,122 +10,122 @@ char *username;
 char *password;
 int perform_store_sql_credentials(char* host_addr, char* user, char* pass)
 {
-    host = host_addr;
-    username = user;
-    password = pass;
-    int ret = 0;
-    MYSQL_RES *result;
-    if(jnx_sql_interface_setup() != 0)
-    {
-        print_streams(JNX_COL_RED,"Error connecting to sql\n");
-        print_streams(DEFAULTCOLOR,"Error connecting to sql in perform_store_sql_credentials\n");
-        return 1;
-    }
-    ret = jnx_sql_resultfill_query("use AUTOMATION; select 'test';",&result);
-    if(ret == 0 ) { mysql_free_result(result); jnx_sql_close();};
-    return ret;
+	host = host_addr;
+	username = user;
+	password = pass;
+	int ret = 0;
+	MYSQL_RES *result;
+	if(jnx_sql_interface_setup() != 0)
+	{
+		print_streams(JNX_COL_RED,"Error connecting to sql\n");
+		print_streams(DEFAULTCOLOR,"Error connecting to sql in perform_store_sql_credentials\n");
+		return 1;
+	}
+	ret = jnx_sql_resultfill_query("use AUTOMATION; select 'test';",&result);
+	if(ret == 0 ) { mysql_free_result(result); jnx_sql_close();};
+	return ret;
 }
 void jnx_sql_close(void )
 {
-    if(connection == NULL) return;
-    mysql_close(connection);
-    connection = NULL;
+	if(connection == NULL) return;
+	mysql_close(connection);
+	connection = NULL;
 }
 int jnx_sql_interface_setup()
 {
-    connection = mysql_init(connection);
-    if(connection == NULL) return 1;
-    return 0;
+	connection = mysql_init(connection);
+	if(connection == NULL) return 1;
+	return 0;
 }
 int jnx_sql_query(char* query,void (*sql_callback)(MYSQL_RES*))
 {
-    
-    if(connection == NULL) return 1;
-	
-/* multi statements is useful for giving a string of commmands that are delimited with ; */
-    if(mysql_real_connect(connection,host,username,password,0,0,NULL, CLIENT_MULTI_STATEMENTS) == NULL){ printf("CONNECTION ERROR\n"); return 1;};    
-    int status = mysql_query(connection,query);
-    if(status)
-    {
-        mysql_close(connection);
-        printf("Problem with query\n");
-        return 1;
-    }
-    /* process each statement result */
-    do 
-    {
-        /* did current statement return data? */
-        result = mysql_store_result(connection);
-        if(result)
-        {
-            /* yes; process rows and free the result set */
-            (*sql_callback)(result);
-            mysql_free_result(result);
-        }
-        else          /* no result set or error */
-        {
-            if (mysql_field_count(connection) == 0)
-            {
-            }
-            else  /* some error occurred */
-            {
-                printf("Could not retrieve result set\n");
-                break;
-            }
-        }
-        /* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
-        if ((status = mysql_next_result(connection)) > 0)
-            printf("Could not execute statement in jnx_sql_query\n");
-    } while (status == 0);
-    return 0;
+
+	if(connection == NULL) return 1;
+
+	/* multi statements is useful for giving a string of commmands that are delimited with ; */
+	if(mysql_real_connect(connection,host,username,password,0,0,NULL, CLIENT_MULTI_STATEMENTS) == NULL){ printf("CONNECTION ERROR\n"); return 1;};    
+	int status = mysql_query(connection,query);
+	if(status)
+	{
+		mysql_close(connection);
+		printf("Problem with query\n");
+		return 1;
+	}
+	/* process each statement result */
+	do 
+	{
+		/* did current statement return data? */
+		result = mysql_store_result(connection);
+		if(result)
+		{
+			/* yes; process rows and free the result set */
+			(*sql_callback)(result);
+			mysql_free_result(result);
+		}
+		else          /* no result set or error */
+		{
+			if (mysql_field_count(connection) == 0)
+			{
+			}
+			else  /* some error occurred */
+			{
+				printf("Could not retrieve result set\n");
+				break;
+			}
+		}
+		/* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
+		if ((status = mysql_next_result(connection)) > 0)
+			printf("Could not execute statement in jnx_sql_query\n");
+	} while (status == 0);
+	return 0;
 }
 int jnx_sql_resultfill_query(char *query, MYSQL_RES **resultptr)
 {
-    if(connection == NULL) 
+	if(connection == NULL) 
 	{
 		jnx_term_printf_in_color(JNX_COL_RED,"Connecting to jnx_sql_resultfill_query is null\n");
 		return 1;}
-    /* multi statements is useful for giving a string of commmands that are delimited with ; */
-if(mysql_real_connect(connection,host,username,password,0,0,NULL, CLIENT_MULTI_STATEMENTS) != connection)
-{ 
-	jnx_term_printf_in_color(JNX_COL_RED,"Connection error in jnx_sql_resultfill_query - Your request: %s\n",query);
-	if(strcmp(query,"") == 0 ) { printf("query is empty\n");  } ;	
-	/*-----------------------------------------------------------------------------
-	 *  May require another return code for unsuccessful connection
-	 *-----------------------------------------------------------------------------*/
-	return 1;
-}
-    int status = mysql_query(connection,query);
-    if(status)
-    {
-        mysql_close(connection);
-        printf("Problem with query\n");
-        return 1;
-    }
-    /* process each statement result */
-    do 
-    {
-        /* did current statement return data? */
-        result = mysql_store_result(connection);
-        if(result)
-        {
-            /* yes; process rows and free the result set */
-            (*resultptr) = result;
-        }
-        else          /* no result set or error */
-        {
-            if (mysql_field_count(connection) == 0)
-            {    
-            }
-            else  /* some error occurred */
-            {
-                printf("Could not retrieve result set\n");
-                break;
-            }
-        }
-        /* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
-        if ((status = mysql_next_result(connection)) > 0)
-            printf("Could not execute statement in jnx_sql_resultfill_query\n");
-    } while (status == 0);
-    return 0;
+	/* multi statements is useful for giving a string of commmands that are delimited with ; */
+	if(mysql_real_connect(connection,host,username,password,0,0,NULL, CLIENT_MULTI_STATEMENTS) != connection)
+	{ 
+		jnx_term_printf_in_color(JNX_COL_RED,"Connection error in jnx_sql_resultfill_query - Your request: %s\n",query);
+		if(strcmp(query,"") == 0 ) { printf("query is empty\n");  } ;	
+		/*-----------------------------------------------------------------------------
+		 *  May require another return code for unsuccessful connection
+		 *-----------------------------------------------------------------------------*/
+		return 1;
+	}
+	int status = mysql_query(connection,query);
+	if(status)
+	{
+		mysql_close(connection);
+		printf("Problem with query\n");
+		return 1;
+	}
+	/* process each statement result */
+	do 
+	{
+		/* did current statement return data? */
+		result = mysql_store_result(connection);
+		if(result)
+		{
+			/* yes; process rows and free the result set */
+			(*resultptr) = result;
+		}
+		else          /* no result set or error */
+		{
+			if (mysql_field_count(connection) == 0)
+			{    
+			}
+			else  /* some error occurred */
+			{
+				printf("Could not retrieve result set\n");
+				break;
+			}
+		}
+		/* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
+		if ((status = mysql_next_result(connection)) > 0)
+			printf("Could not execute statement in jnx_sql_resultfill_query\n");
+	} while (status == 0);
+	return 0;
 }
--- src/main.c
+++ src/main.c
@@ -20,91 +20,91 @@ enum ERROR_CODE { SER_UP, ARG_UP,SQL_UP };
 #define DEFAULTCOLOR -1
 void usage()
 {
-    print_streams(DEFAULTCOLOR,"Satellite is a half duplex server/client in one for transmission of several shell commands\n");
-    print_streams(DEFAULTCOLOR,"No mode selected, please try again using -m [TRANSMIT,RECEIVE]\n");
-    print_streams(DEFAULTCOLOR,"--conf [PATH TO CONF FILE]\n");
+	print_streams(DEFAULTCOLOR,"Satellite is a half duplex server/client in one for transmission of several shell commands\n");
+	print_streams(DEFAULTCOLOR,"No mode selected, please try again using -m [TRANSMIT,RECEIVE]\n");
+	print_streams(DEFAULTCOLOR,"--conf [PATH TO CONF FILE]\n");
 }
 void catch_int (int signum) 
 { 
-    pid_t my_pid;
-    print_streams(DEFAULTCOLOR,"\nReceived an interrupt! About to exit ..\n");
+	pid_t my_pid;
+	print_streams(DEFAULTCOLOR,"\nReceived an interrupt! About to exit ..\n");
 
-    jnx_network_cancel_listener();
-    fflush(stdout);
-    my_pid = getpid();
-    kill(my_pid, SIGKILL);	
-    system("sudo killall -9 satellite");
+	jnx_network_cancel_listener();
+	fflush(stdout);
+	my_pid = getpid();
+	kill(my_pid, SIGKILL);	
+	system("sudo killall -9 satellite");
 }
 int main(int argc, char **argv) 
 {
-    //Register for signal handling
-    signal(SIGINT, catch_int);
-    static struct option long_options[] = 
-    {
-        {"mode",required_argument,0,'m'},
-        {"conf",required_argument,0,'c'},
-        {0,      0,                 0, 0 }
-    };
-    int option_index = 0;
-    int i;
-    int port;
-    char *conf = NULL;
-    char* mode = NULL;
-    while(( i = getopt_long_only(argc,argv,"c:m:",long_options,&option_index)) != -1)
-    {
-        switch(i)
-        {
-            case 'c':
-                conf = optarg;
-                break;
-            case 'm':
-                mode = optarg;
-                break;
-            default:
-                usage();
-                exit(1);
-        }
-    }
-    if(conf == NULL || mode == NULL)
-    {
-        usage();
-        return ARG_UP;
-    }
-    config = utils_set_configuration(conf);
-    /*-----------------------------------------------------------------------------
-     *  Setup our log
-     *-----------------------------------------------------------------------------*/
-    if(jnx_log_setup(jnx_hash_get(config,"logpath")) != 0)
-    {
-        jnx_term_printf_in_color(JNX_COL_RED,"WARNING: Could not start logger\n");
-        return 1;
-    }
-    print_streams(JNX_COL_GREEN,"Satellite Started\n");
-    print_streams(DEFAULTCOLOR,"Storing SQL credentials temporarily\n");
-    /*-----------------------------------------------------------------------------
-     *  Store sql credentials
-     *-----------------------------------------------------------------------------*/
-    if(perform_store_sql_credentials(jnx_hash_get(config,"sqlhost"),jnx_hash_get(config,"sqluser"),jnx_hash_get(config,"sqlpass")) != 0)
-    {
-        print_streams(JNX_COL_RED,"Error with sql credentials\n");
-        return 1;
-    }
-    if(strcmp(mode,"RECEIVE") == 0)
-    {
-        if(!jnx_hash_get(config,"listenport"))
-        { print_streams(JNX_COL_RED,"Requires port number, option -p\n");return 1; };
-        if(jnx_start_listener(jnx_hash_get(config,"listenport")) != 0)
-        {
-            print_streams(JNX_COL_RED,"Error starting the listener\n");
-            return 1;
-        }
-    }
-    if(strcmp(mode,"TRANSMIT") == 0)
-    {
-        jnx_start_transmitter();        
-    }
-    jnx_hash_delete(config);
-    return 0;
+	//Register for signal handling
+	signal(SIGINT, catch_int);
+	static struct option long_options[] = 
+	{
+		{"mode",required_argument,0,'m'},
+		{"conf",required_argument,0,'c'},
+		{0,      0,                 0, 0 }
+	};
+	int option_index = 0;
+	int i;
+	int port;
+	char *conf = NULL;
+	char* mode = NULL;
+	while(( i = getopt_long_only(argc,argv,"c:m:",long_options,&option_index)) != -1)
+	{
+		switch(i)
+		{
+			case 'c':
+				conf = optarg;
+				break;
+			case 'm':
+				mode = optarg;
+				break;
+			default:
+				usage();
+				exit(1);
+		}
+	}
+	if(conf == NULL || mode == NULL)
+	{
+		usage();
+		return ARG_UP;
+	}
+	config = utils_set_configuration(conf);
+	/*-----------------------------------------------------------------------------
+	 *  Setup our log
+	 *-----------------------------------------------------------------------------*/
+	if(jnx_log_setup(jnx_hash_get(config,"logpath")) != 0)
+	{
+		jnx_term_printf_in_color(JNX_COL_RED,"WARNING: Could not start logger\n");
+		return 1;
+	}
+	print_streams(JNX_COL_GREEN,"Satellite Started\n");
+	print_streams(DEFAULTCOLOR,"Storing SQL credentials temporarily\n");
+	/*-----------------------------------------------------------------------------
+	 *  Store sql credentials
+	 *-----------------------------------------------------------------------------*/
+	if(perform_store_sql_credentials(jnx_hash_get(config,"sqlhost"),jnx_hash_get(config,"sqluser"),jnx_hash_get(config,"sqlpass")) != 0)
+	{
+		print_streams(JNX_COL_RED,"Error with sql credentials\n");
+		return 1;
+	}
+	if(strcmp(mode,"RECEIVE") == 0)
+	{
+		if(!jnx_hash_get(config,"listenport"))
+		{ print_streams(JNX_COL_RED,"Requires port number, option -p\n");return 1; };
+		if(jnx_start_listener(jnx_hash_get(config,"listenport")) != 0)
+		{
+			print_streams(JNX_COL_RED,"Error starting the listener\n");
+			return 1;
+		}
+	}
+	if(strcmp(mode,"TRANSMIT") == 0)
+	{
+		jnx_start_transmitter();        
+	}
+	jnx_hash_delete(config);
+	return 0;
 }
 
 
