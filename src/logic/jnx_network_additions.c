398b822b3415deb113aea1b9b80decae74d7da66
Caspar <admin@caspar.local>
2013-04-03 16:16:51 +0100

    working prototype state, untested

--- src/logic/jnx_network_additions.c
+++ src/logic/jnx_network_additions.c
@@ -28,7 +28,7 @@ void jnx_network_post_file_callback(char *message)
 int file_size(char *path)
 {
 
-	system("pwd");
+
 	printf("Checking file size of %s\n", path);
 	FILE *fp = NULL;
 
@@ -60,7 +60,7 @@ int jnx_network_post_file(char *filepath, char *jobid)
         return 1;
     }
 
-    char *rel_path = "/uploads/%s/%s";
+    char *rel_path = "/upload/%s/%s";
 	char relative_path_buffer[256];
 	snprintf(relative_path_buffer,256,rel_path,jobid,filepath);
 	printf("jnx_network_post_file relative path : %s\n",relative_path_buffer);
@@ -76,6 +76,18 @@ int jnx_network_post_file(char *filepath, char *jobid)
 	snprintf(sendline,MAXBUFFER,post,relative_path_buffer,jnx_hash_get(config,"frontendserver"),jnx_hash_get(config,"frontendport"),size);
 	
 	printf("TOTAL RESULT %s\n",sendline);
-
+	size_t msgsize = strlen(sendline) + size + 1;
+    char *buffy = malloc(msgsize);
+    bzero(buffy, msgsize);
+    strcpy(buffy, sendline);
+    strcat(buffy, fcont);
+	
+	jnx_network_send_message_callback c = jnx_network_post_file_callback;
+    if(jnx_network_send_message(jnx_hash_get(config,"frontendserver"),atoi(jnx_hash_get(config,"frontendport")),buffy, c) == 1)
+	{
+		printf("Failed to transmit %s\n",filepath);
+		return 1;
+	}
+	free(buffy);
 	return 0;
 }
