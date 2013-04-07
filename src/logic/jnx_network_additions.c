2832a716a95da8d57813bd36b896e52a18c949ae
AlexsJones <alexsimonjones@gmail.com>
2013-04-07 20:06:59 +0100

    free fcont from jnx_file_read

--- src/logic/jnx_network_additions.c
+++ src/logic/jnx_network_additions.c
@@ -27,8 +27,6 @@ void jnx_network_post_file_callback(char *message)
 }
 int file_size(char *path)
 {
-
-
 	printf("Checking file size of %s\n", path);
 	FILE *fp = NULL;
 
@@ -51,43 +49,41 @@ int jnx_network_post_file(char *filepath, char *jobid)
 {
 	char sendline[MAXBUFFER];
 	bzero(&sendline, MAXBUFFER);
-    
-    char *fcont = jnx_file_read(filepath); 
-    int size = file_size(filepath);
-    if ( size < 0 )
-    {
-        printf("send_file_to_server error in determining the size of the file...");
-        return 1;
-    }
 
-    char *rel_path = "/upload/%s/%s";
+	char *fcont = jnx_file_read(filepath); 
+	int size = file_size(filepath);
+	if ( size < 0 )
+	{
+		printf("send_file_to_server error in determining the size of the file...");
+		return 1;
+	}
+
+	char *rel_path = "/upload/%s/%s";
 	char relative_path_buffer[256];
 	snprintf(relative_path_buffer,256,rel_path,jobid,filepath);
 	printf("jnx_network_post_file relative path : %s\n",relative_path_buffer);
 
 	char *post = "POST %s HTTP/1.1\r\n" 
-                 "Host: %s:%s\r\n"
-                 "Content-Type: text/plain\r\n"
-                 "Content-Length: %d\r\n"
-                 "\r\n";
-	
-	
+		"Host: %s:%s\r\n"
+		"Content-Type: text/plain\r\n"
+		"Content-Length: %d\r\n"
+		"\r\n";
 
 	snprintf(sendline,MAXBUFFER,post,relative_path_buffer,jnx_hash_get(config,"frontendserver"),jnx_hash_get(config,"frontendport"),size);
-	
 	printf("TOTAL RESULT %s\n",sendline);
 	size_t msgsize = strlen(sendline) + size + 1;
-    char *buffy = malloc(msgsize);
-    bzero(buffy, msgsize);
-    strcpy(buffy, sendline);
-    strcat(buffy, fcont);
-	
+	char *buffy = malloc(msgsize);
+	bzero(buffy, msgsize);
+	strcpy(buffy, sendline);
+	strcat(buffy, fcont);
+
 	jnx_network_send_message_callback c = jnx_network_post_file_callback;
-    if(jnx_network_send_message(jnx_hash_get(config,"frontendserver"),atoi(jnx_hash_get(config,"frontendport")),buffy, c) == 1)
+	if(jnx_network_send_message(jnx_hash_get(config,"frontendserver"),atoi(jnx_hash_get(config,"frontendport")),buffy, c) == 1)
 	{
 		printf("Failed to transmit %s\n",filepath);
 		return 1;
 	}
 	free(buffy);
+	free(fcont);
 	return 0;
 }
